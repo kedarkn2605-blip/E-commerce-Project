b6e2792e68d32dc55a3ff0b96cf1d34e7e92c75b
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:43:57 +0530

    Update product.sql

--- sql_insert_values_code/product.sql
+++ sql_insert_values_code/product.sql
@@ -1,1 +1,11 @@
+INSERT INTO product VALUES (1,'Smartphone',18000,50,'Samsung',1,1);
+INSERT INTO product VALUES (2,'Laptop',55000,20,'HP',2,2);
+INSERT INTO product VALUES (3,'Mouse',499,100,'Logitech',3,3);
+INSERT INTO product VALUES (4,'Men Shirt',1299,40,'Allen Solly',4,4);
+INSERT INTO product VALUES (5,'Women Kurti',999,30,'Biba',5,5);
+INSERT INTO product VALUES (6,'Sports Shoes',2999,25,'Puma',6,6);
+INSERT INTO product VALUES (7,'Mixer Grinder',3500,15,'Prestige',7,7);
+INSERT INTO product VALUES (8,'Text Book',650,60,'Oxford',8,8);
+INSERT INTO product VALUES (9,'Smart Watch',4500,35,'Noise',9,9);
+INSERT INTO product VALUES (10,'Gaming Mouse',2499,20,'Razer',10,10);
 
