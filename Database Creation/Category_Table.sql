060f52f716978dc69ed4bb30eb9675d8774f5ceb
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:07:41 +0530

    Update Category_Table.sql

--- Database Creation/Category_Table.sql
+++ Database Creation/Category_Table.sql
@@ -1,5 +1,5 @@
-create table e_commerce.category (
-category_id int primary key,
-category_name varchar(255),
-Description varchar(255)
+CREATE TABLE category (
+  category_id NUMBER PRIMARY KEY,
+  category_name VARCHAR2(255),
+  description VARCHAR2(255)
 );
