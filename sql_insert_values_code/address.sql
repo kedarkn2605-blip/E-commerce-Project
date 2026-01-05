128813df69e83d85d7909c94f10666ad9d2b0cae
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:23:48 +0530

    Update address.sql

--- sql_insert_values_code/address.sql
+++ sql_insert_values_code/address.sql
@@ -1,1 +1,11 @@
+INSERT INTO address VALUES (1,'12A','Shivaji Apartments','Tilakwadi','Karnataka','Belgaum',590006,1);
+INSERT INTO address VALUES (2,'8','Nimbalkar House','Raviwar Peth','Maharashtra','Solapur',413001,2);
+INSERT INTO address VALUES (3,'21','Patil Nivas','Market Road','Maharashtra','Kolhapur',416003,3);
+INSERT INTO address VALUES (4,'45','Naik Residency','Vidyanagar','Karnataka','Hubballi',580031,4);
+INSERT INTO address VALUES (5,'10','Deshpande Villa','Civil Lines','Maharashtra','Pune',411001,5);
+INSERT INTO address VALUES (6,'7B','Kulkarni Heights','Station Road','Karnataka','Dharwad',580001,6);
+INSERT INTO address VALUES (7,'19','Joshi Nivas','College Road','Maharashtra','Nashik',422005,7);
+INSERT INTO address VALUES (8,'33','Kamble Residency','MG Road','Karnataka','Belgaum',590001,8);
+INSERT INTO address VALUES (9,'5','Chavan Palace','Shivaji Chowk','Maharashtra','Satara',415001,9);
+INSERT INTO address VALUES (10,'14','Hegde Bhavan','Keshwapur','Karnataka','Hubballi',580023,10);
 
